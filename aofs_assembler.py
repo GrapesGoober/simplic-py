a7d87d9556bfeb421f261dfd650646400f567862
Graps <88650896+GrapesGoober@users.noreply.github.com>
2023-09-26 21:48:46 +0700

    Made an instr-parser pair

--- aofs_assembler.py
+++ aofs_assembler.py
@@ -1,5 +1,31 @@
 import aofs_mnemonics
 
+instructions = [
+    # Conditional instructions
+    ("move", 	parse_conditional_operands),
+    ("cadd", 	parse_conditional_operands),
+    
+    # Memory instructions, with 4-bit immediate post indexing
+    ("load", 	parse_indexing_operands),
+    ("store", 	parse_indexing_operands),
+    
+    ("insert",	parse_insert_operands),
+    
+    # ALU instructions
+    ("shift", 	parse_alu_operands),
+    ("add", 	parse_alu_operands),
+    ("sub", 	parse_alu_operands),
+    ("mul", 	parse_alu_operands),
+    ("longmul",	parse_alu_operands),
+    ("divide", 	parse_alu_operands),
+    ("mod", 	parse_alu_operands),
+    ("and", 	parse_alu_operands),
+    ("or", 		parse_alu_operands),
+    ("xor", 	parse_alu_operands),
+    ("nor" 		parse_alu_operands)
+]
+
+
 def asmfile_to_hexfile(filename : str) -> None:
     with open(filename, mode = "r") as file:
         for line in file:
@@ -20,3 +46,14 @@ def parse_line(asmline : str) -> int:
 
     # parse each type differently
     
+def parse_conditional_operands(tokens : list) -> int:
+    pass
+    
+def parse_indexing_operands(tokens : list) -> int:
+    pass
+    
+def parse_insert_operands(tokens : list) -> int:
+    pass
+    
+def parse_alu_operands(tokens : list) -> int:
+	pass
