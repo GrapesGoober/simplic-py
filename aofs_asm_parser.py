215ac79bf8e79e6e54535cae4af600a0a64e4463
Graps <twinklierre@gmail.com>
2023-09-27 13:38:18 +0700

    Finished asm token parser

--- aofs_asm_parser.py
+++ aofs_asm_parser.py
@@ -1,7 +1,18 @@
+# Defining a set of instruction types and their instructions
+instructions = {
+    "cond" : [ "move", "cadd" ],
+    "memory" : [ "load", "store" ],
+    "insert" : [ "insert" ],
+    "shift" : [ "shift" ],
+    "alu" : [ 
+        "add", "sub", "mul", "longmul", "div", "mod", 
+        "and", "or", "xor", "nor"
+    ]
+}
 
-
-def parse_reg(token : str) -> int:
-    registers = [
+# Defining a set of operand tokens (excluding instructions)
+token_set = {
+    "register" : [
         # A special zero value for resetting registers
         "zero",
         # General purpose registers
@@ -15,49 +26,78 @@ def parse_reg(token : str) -> int:
         "address",  # Address register  - for memory and jumps
         "link",     # Link register     - keep progc addresses to return to
         "progc"     # Program counter   - current instruction to execute
-    ]
+    ],
+    "condition" : [
 
-def parse_cond(token : str) -> int:
-    conditions = [
-
-    ]
-
-def parse_imm4(token : str) -> int:
-    pass
-
-def parse_imm8(token : str) -> int:
-    pass
-
-def parse_sft(token : str) -> int:
-    shift_type = [
+    ],
+    "shift-op" : [
         "left",
         "right",
         "arith",
         "rotate"
     ]
+}
 
-# Retrieve the instruction index and a set of functions to parse the operands
-def parse_instr(token : str) -> int:
-    # Define a set of instruction types and their instructions
-    instructions = {
-        "cond" : [ "move", "cadd" ],
-        "memory" : [ "load", "store" ],
-        "insert" : [ "insert" ],
-        "shift" : [ "shift" ],
-        "alu" : [ 
-            "add", "sub", "mul", "longmul", "div", "mod", 
-            "and", "or", "xor", "nor"
-        ]
-    }
-    # Defining the set of functions to parse operands for each instruction type
-    operands = {
-        "cond" : [ parse_reg, parse_reg, parse_cond ],
-        "memory" : [ parse_reg, parse_reg, parse_imm4 ],
-        "insert" : [ parse_reg, parse_imm8 ],
-        "shift" : [ parse_reg, parse_sft, parse_reg ],
-        "alu" : [ parse_reg, parse_reg, parse_reg, parse_reg ]
-    }
+# Returns a function to parse token with a specified type
+def parse_token(token_type : str) -> int:
 
+    def parser(token : str) -> int:
+        if token.lower() not in token_set[token_type]:
+            raise Exception(f"Unrecognized {token_type} '{token}'")
+        else:
+            return token_set[token_type].index(token.lower())
+    
+    return parser
 
+# Returns a function to parse immediate valuue with the specified bitsize limitation
+def parse_imm(bit_size : int):
 
+    def parser(token : str) -> int:
+        result = 0
+        try:
+            if token.startswith("0x"):
+                result = int(token, 16)
+            elif token.startswith("0b"): 
+                result = int(token, 2)
+            else:
+                result = int(token, 10)
+        except:
+            raise Exception(f"Invalid immediate '{token}'")
+
+        if result.bit_length() > bit_size:
+            raise Exception(f"Immediate value '{token}' too big for {bit_size} bits.")
+        
+        return result
+    
+    return parser
+
+# Defining how each types of instruction recieve operands
+# Key   : Instruction type
+# Value : A list of parser functions specific to that function type
+operands = {
+    "cond" : [ 
+        parse_token("register"), # Rd   - Destination Register
+        parse_token("register"), # Rn   - Input Register 1
+        parse_token("condition") # CND  - Condition
+    ],
+    "memory" : [ 
+        parse_token("register"), # Rd   - Destination Register
+        parse_token("register"), # Rn   - Address
+        parse_imm(4)             # Imm4 - Post Addressing
+    ],
+    "insert" : [ 
+        parse_token("register"), # Rd   - Destination Register
+        parse_imm(8)             # Imm8 - Immediate value to insert
+    ],
+    "shift" : [ 
+        parse_token("register"), # Rd   - Destination Register
+        parse_token("shift-op"), # Op   - Shifting operation
+        parse_token("register")  # Rm   - Distance to shift
+    ],
+    "alu" : [ 
+        parse_token("register"), # Rd - Destination Register
+        parse_token("register"), # Rn - Input Register 1
+        parse_token("register"), # Rm - Input Register 2
+    ]
+}
 
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import sys, aofs_assembler
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        raise Exception("Requires second argument for file path")
+    # if len(sys.argv) != 2:
+    #     raise Exception("Requires second argument for file path")
     
-    aofs_assembler.asmfile_to_hexfile(sys.argv[1])
-
-
+    # aofs_assembler.asmfile_to_hexfile(sys.argv[1])
+    pass
