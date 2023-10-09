60f542ab51d2483721e8ef8b42b49067b19782af
Graps <twinklierre@gmail.com>
2023-10-09 16:17:39 +0700

    Finished no-except refactoring

--- assembler/parser.py
+++ assembler/parser.py
@@ -1,24 +1,30 @@
 from assembler.token_set import token_set
 
 # Parse a mnemonic into binary code (essentially just an index-of)
-def parse_mnemonic(token : str, token_type : str) -> tuple(bool, any):
+def parse_mnemonic(token : str, token_type : str) -> int or str:
 
     if token_type not in token_set:
-        return False, f"Token type {token_type} does not exist."
+        print(f"Error: Token type {token_type} does not exist.")
+        return False
         
     token_list = token_set[token_type]
     if token.lower() in token_list:
         token_index = token_list.index(token.lower())
-        return True, token_index
+        return token_index
     elif token == "":
-        return False, f"Expected a {token_type} before newline."
+        print(f"Error: Expected a {token_type} before newline.")
+        return False
     else:
-        return False, f"Unrecognized {token_type} '{token}'."
+        print(f"Error: Unrecognized {token_type} '{token}'.")
+        return False
 
 # Parse an immediate into integer with the specified bitsize
 def parse_immediate(token : str, bit_size : int) -> int:
 
     result = 0
+    if token == "":
+        print(f"Error: Expected a {bit_size}-bit immediate.")
+        return False
     try:
         if token.lower().startswith("0x"):
             result = int(token, 16)
@@ -27,15 +33,14 @@ def parse_immediate(token : str, bit_size : int) -> int:
         else:
             result = int(token, 10)
     except ValueError:
-        if token == "":
-            return False, f"Expected a {bit_size}-bit immediate before newline."
-        else:
-            return False, f"Invalid immediate '{token}'."
+        print(f"Error: Invalid immediate '{token}'.")
+        return False
 
     if result.bit_length() > bit_size:
-        return False, f"Immediate value '{token}' too big for {bit_size} bits."
-
-    return True, result
+        print(f"Error: Immediate value '{token}' too big for {bit_size} bits.")
+        return False
+    
+    return result
 
 # Parses a line of assembly to binary code
 def parse_instruction(asmline : str) -> list[int]:
@@ -44,39 +49,43 @@ def parse_instruction(asmline : str) -> list[int]:
     tokens = asmline.split() + [""] * 4
 
     # firstly, parse the opcode and destination register
-    status, opcode = parse_mnemonic(tokens[0], "instruction")
-    if not status: return status, opcode
-    status, rd = parse_mnemonic(tokens[1], "register") 
-    if not status: return status, rd
+    opcode = parse_mnemonic(tokens[0], "instruction")
+    rd = parse_mnemonic(tokens[1], "register") 
     
     # next, parse the operands
-    operands = 0
+    operandA, operandB = 0, 0
     # parsing operands are finicky since syntax is specific to opcode
     if opcode == 0: # start with conditional move "cmove"
-        operands += parse_mnemonic(tokens[2], "register") << 4
-        operands += parse_mnemonic(tokens[3], "condition")
+        operandA = parse_mnemonic(tokens[2], "register")
+        operandB = parse_mnemonic(tokens[3], "condition")
     elif opcode == 1: # count leading zeros 
-        operands += parse_mnemonic(tokens[2], "register") << 4
+        operandB += parse_mnemonic(tokens[2], "register")
         if tokens[3] != "": 
-            return False, f"Unexpected operand '{tokens[3]}'"
+            print(f"Error: Unexpected operand '{tokens[3]}'")
+            return False
     elif opcode in [2, 3]: # memory instructions ("load" and "store")
-        operands += parse_mnemonic(tokens[2], "register") << 4
-        operands += parse_immediate(tokens[3], 4)
+        operandA = parse_mnemonic(tokens[2], "register")
+        operandB = parse_immediate(tokens[3], 4)
     elif opcode == 4: # insert instruction takes 8-bit immediate
-        operands += parse_immediate(tokens[2], 8)
+        operandB += parse_immediate(tokens[2], 8)
         if tokens[3] != "": 
-            return False, f"Unexpected operand '{tokens[3]}'"
+            print(f"Error: Unexpected operand '{tokens[3]}'")
+            return False
     elif opcode == 5: # shift instruction takes a special "shift operation" token type
-        operands += parse_mnemonic(tokens[2], "shift operation") << 4
-        operands += parse_mnemonic(tokens[3], "register")
+        operandA = parse_mnemonic(tokens[2], "shift operation")
+        operandB = parse_mnemonic(tokens[3], "register")
     else: # rest ALU instructions take registers
-        operands += parse_mnemonic(tokens[2], "register") << 4
-        operands += parse_mnemonic(tokens[3], "register")
+        operandA = parse_mnemonic(tokens[2], "register")
+        operandB = parse_mnemonic(tokens[3], "register")
 
     if tokens[4] != "": 
-        return False, f"Unexpected operand '{tokens[4]}'"
+        print(f"Error: Unexpected operand '{tokens[4]}'")
+        return False
     
-    return [(opcode << 4) + rd, operands]
+    if any(i is False for i in [opcode, rd, operandA, operandB]):
+        return False
+
+    return [(opcode << 4) + rd, (operandA << 4) + operandB]
 
 def parse(asmlines : list[str]) -> dict[str: int]:
 
@@ -90,27 +99,31 @@ def parse(asmlines : list[str]) -> dict[str: int]:
         # split, but also add some empty padding tokens for safe indexing
         tokens = asmline.split() + [""] * 4
 
-        # try parsing the code
-        status, result = False, None
+        # try parsing the code onto "result"
+        result = None
         if tokens[0].lower() == "move":
-            bytecodes += parse_instruction(f"cmove {tokens[1]} {tokens[2]} always") 
+            result = parse_instruction(f"cmove {tokens[1]} {tokens[2]} always") 
             if tokens[3] != "":
-                status, result = False, f"Unexpected token '{tokens[3]}'"
+                print(f"Error: Unexpected token '{tokens[3]}'")
+                return False
         elif tokens[0].lower() == "flag":
             if tokens[1].lower() == "on":
-                status, result = parse_instruction("nor zero zero zero")
+                result = parse_instruction("nor zero zero zero")
             elif tokens[1].lower() == "reset":
-                status, result = parse_instruction("xor zero zero zero")
+                result = parse_instruction("xor zero zero zero")
             else:
-                status, result = False, f"Unrecognized flag directive {tokens[1]}"
+                print(f"Error: Unrecognized flag directive '{tokens[1]}'")
+                return False
             if tokens[2] != "":
-                status, result = False, f"Unexpected token '{tokens[2]}'"
+                print(f"Error: Unexpected token '{tokens[3]}'")
+                return False
         else:
-            bytecodes += parse_instruction(asmline)
+            result = parse_instruction(asmline)
 
-        if status:
+        if result:
             bytecodes += result
         else:    
-            return {False: f"Assembler error at line {i+1}: {result}"}
+            print(f"Error: Assembler error at line {i+1}\n\t {asmline}")
+            return False
         
     return { i: v for i, v, in enumerate(bytecodes) }
