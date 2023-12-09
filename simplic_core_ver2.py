83d0b5beccc0928f5468de9c16e5724090ce8fa0
Graps <twinklierre@gmail.com>
2023-12-09 19:37:41 +0700

    setup microcontroller implementation

---
+++ simplic_core_ver2.py
@@ -0,0 +1,45 @@
+# Simplic Microcontroller - Author Nachat K (grapsgoober)
+# This is an implementation of Simplic, a 16-bit accumulator-based microcontroller. 
+
+class Memory:
+    words = {}
+    def read(self, address):
+        return self.words[address] if address in self.words else 0
+    def write(self, address, value):
+        self.words[address] = value
+
+class SimplicCore:
+    BITSIZE = 16
+
+    # Internal registers
+    ACC, BSP, PC = 0, 0, 0
+    Z, N, C, V = 0, 0, 0, 0
+
+    # Mux & Signal bus
+    result_mux = {i:0 for i in range(16)}
+    constrol_signals = {}
+
+    def decode(self, opcode, control):
+        pass
+
+    def compute(self, data):
+
+        self.result_mux[0x6] = self.ACC + data
+        self.result_mux[0x7] = self.ACC - data
+        self.result_mux[0x8] = self.ACC * data
+        self.result_mux[0x9] = (self.ACC * data) >> self.BITSIZE
+        self.result_mux[0xA] = self.ACC // data
+        self.result_mux[0xB] = self.ACC % data
+        self.result_mux[0xC] = 0 # not sure what to do here
+        self.result_mux[0xD] = self.ACC & data
+        self.result_mux[0xE] = self.ACC | data
+        self.result_mux[0xF] = ~data
+
+    def multiplex(self, opcode):
+        opcode &= (1 << self.BITSIZE) - 1
+        self.ACC = self.result_mux[opcode] if self.ACC_write_en else self.ACC
+        self.BSP = self.result_mux[opcode] if self.BSP_write_en else self.BSP
+        self.PC = self.result_mux[opcode] if self.PC_write_en else self.PC
+    
+    def execute(self, instruction):
+        pass
