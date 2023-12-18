a88627c13c323682c9ea40249a6cb1f76461d99b
Graps <twinklierre@gmail.com>
2023-12-18 20:56:37 +0700

    prototyping clz bitmapping logic

--- simplic/main.py
+++ simplic/main.py
@@ -12,6 +12,25 @@ def assemble_instructions():
     destination = "simplic\\test_codes\\fib.hex"
     assembler.asm_to_hex(source, destination)
 
+def count_lz():
+    clz = lambda: len(f"{V1:016b}".split('1')[0])
+
+    A = 0
+    V1 = 0x00F1
+    A = clz()
+    print(f"{V1:016b}")
+
+    V2 = 0x0001
+    A -= V2
+    V3 = A
+    A = V2
+    A <<= V3
+
+    V4 = A
+    A = ~V4 & 0xFFFF
+    A &= V1
+    print(f"{A:016b}")
+
 if __name__ == '__main__':
-    assemble_instructions()
-    
+    #assemble_instructions()
+    count_lz()
