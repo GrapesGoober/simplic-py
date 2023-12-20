44295af5bc843ef6bd7f67cc7700f95eb2dd2204
grapsgoober <twinklierre@gmail.com>
2023-12-20 13:30:42 +0700

    worked on asm

--- simplic/test_codes/fib.asm
+++ simplic/test_codes/fib.asm
@@ -1,12 +1,12 @@
 start:
-    set 0   0   # 'previous' at 0
+    set 0   1   # 'previous' at 0
     set 1   1   # 'current' at 1
-    set 2   2   # 'next' at 2
+    set 2   0   # 'next' at 2
     set 3   2   # 'counter' at 3
     set 4   1   # 'increment' at 4
     set 5   24  # 'max' at 5
 
-fib_loop:
+loop:
     load    0   # next = previous + current    
     add     1
     store   2
@@ -14,13 +14,9 @@ fib_loop:
     store   0
     load    2   # current = next
     store   1
-
-    # compare counter and max, how?
-
     load    3   # counter += increment
     add     4 
     store   3
-    jump    loop
-
-
-    
+    load    3   # compare counter and max
+    sub     5
+    if less: loop
