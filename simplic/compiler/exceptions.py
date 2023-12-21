bd50908d49a3f059be0a227b3881d3bade526743
grapsgoober <twinklierre@gmail.com>
2023-12-21 23:34:05 +0700

    Prettified asm exception

--- simplic/compiler/exceptions.py
+++ simplic/compiler/exceptions.py
@@ -13,7 +13,7 @@ class AsmException(Exception):
                 for i, line in enumerate(f):
                     if line_num - 3 < i < line_num + 2: 
                         err_string += f"  {i+1}:\t{line}"
-            err_string += f"Error at line {line_num + 1}: {self.message}\n"
             print(err_string)
+            print(f"Error at line {line_num + 1}: {self.message}", end='\n\n')
         else:
             print(self.message)
