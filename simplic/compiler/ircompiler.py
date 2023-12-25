185c533846fc9e01f42610c36e2ca126e655d1c7
Graps <twinklierre@gmail.com>
2023-12-25 17:06:10 +0700

    implement stack allocation

--- simplic/compiler/ircompiler.py
+++ simplic/compiler/ircompiler.py
@@ -12,6 +12,7 @@ IR = {
             ('set',     'i', 2),
             ('add',     'j', 'b', 'a'),
             ('add',     'a', 'j', 'i'),
+            ('set',     'k', 'j'),   # variable k should recycle i
             ('cmp',     'a', 20),
             ('if',      'less', 'loop'),
             ('return',  'b'),
@@ -28,42 +29,60 @@ IR = {
 
 code = IR['somefunc'][1]
 
+# capture labels and determine variable live ranges
 labels = {}
-lifetimes = {}
+live_range = {}
 for i, tokens in enumerate(code):
     match tokens[0]:
         case 'call': continue
-        case 'label':
-            labels[tokens[1]] = i
+        case 'label': labels[tokens[1]] = i
         case 'if':
-            if tokens[2] in labels:
-                for v, (start, end) in lifetimes.items():
-                    address = labels[tokens[2]]
-                    if start <= address <= end: lifetimes[v][1] = i
+            if tokens[2] not in labels: continue
+            for v, (start, end) in live_range.items():
+                address = labels[tokens[2]]
+                if start <= address <= end: live_range[v][1] = i
         case _:
             variables = []
             for var in tokens[1:]:
                 if isinstance(var, str):
                     variables.append(var)
             for v in variables:
-                if v in lifetimes: lifetimes[v][1] = i
-                else: lifetimes[v] = [i, i]
+                if v in live_range: live_range[v][1] = i
+                else: live_range[v] = [i, i]
 
-
-alloc = {}
-avail = [0]
-for i in range(len(code)):
-    print(f'{i}\t', end='')
-    for v, (start, end) in lifetimes.items():
+# allocates variables and immediates
+alloc = {} # stores the variable mappings
+immediates = [] # stores the immediate value mappings
+avail = [0] # local variable, stores the current available usage
+for i, tokens in enumerate(code):
+    # if the current line uses variables, have to allocate/deallocate them
+    if tokens[0] in ('call', 'label', 'if'): continue
+    for v in tokens[1:]:
+        if not isinstance(v, str): continue
+        start, end = live_range[v]
         if i == start: # entering lifespan of a variable
             alloc[v] = avail[0]
             if len(avail) == 1: avail[0] += 1
             else: avail.pop(0)
-        elif i == end + 1:  # exiting lifespan of a variable
+        elif i == end:  # exiting lifespan of a variable
             avail.insert(0, alloc[v])
-            alloc.pop(v)
 
-        if v in alloc:
-            print(f'{v}:{alloc[v]}\t', end='')
-        else: print('\t', end='')
+
+
+
+
+
+import json
+print(json.dumps(alloc, indent=2))    
+
+for i, tokens in enumerate(code):
+    if tokens[0] in ('call', 'label', 'if'): 
+        print('---')
+        continue
+    for v in tokens[1:]:
+        if not isinstance(v, str): continue
+        print(f"{v} : {alloc[v]}", end='\t')
     print()
+
+
+
