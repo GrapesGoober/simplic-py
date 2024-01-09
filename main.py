7b3cb5f12dee2195c56ef01df7798a8cbb1c2b36
grapsgoober <twinklierre@gmail.com>
2024-01-09 13:01:14 +0700

    streamlined and standardized compilation dataflow

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from simplic import SimplicVM, SimplicIR, SimplicAsm, SimplicErr, error_print
 
 if __name__ == '__main__':
 
-    hexfile = "test_codes\\fib.hex"
-    
     from test_codes.IRfib import fibbonaci
     ir = SimplicIR(fibbonaci, 'fibbonaci')
-    ir.compile()
+    asmcode = ir.compile()
 
     asm = SimplicAsm()
-    asm.from_list(ir.asm)
-    asm.compile(hexfile)
+    asm.from_list(asmcode)
+    bytecodes = asm.compile()
     # handle error using
     # error_print(asmfile, asm.iter, e.message)
 
     vm = SimplicVM()
-    vm.load_program(hexfile)
+    vm.from_list(bytecodes)
     vm.run()
 
