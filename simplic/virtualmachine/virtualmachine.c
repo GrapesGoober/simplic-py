409b63c54f7de33a16376fd57d705ab0873daae7
grapsgoober <twinklierre@gmail.com>
2024-01-16 14:34:50 +0700

    refactored (prettify) c virtualmachine

--- simplic/virtualmachine/virtualmachine.c
+++ simplic/virtualmachine/virtualmachine.c
@@ -3,47 +3,51 @@
 
 #define WORD_SIZE 16
 
-typedef struct SimplicMicrocontroller {
-    uint8_t instructions[0xFFFF];
+typedef struct SimplicVM {
+    uint8_t  instruction[0xFFFF];
     uint16_t memory[0xFFFF];
-} SimplicMicrocontroller;
+} SimplicVM;
 
+void execute(SimplicVM *vm) {
 
-void execute(SimplicMicrocontroller *sm) {
-    uint16_t* PC = &sm->memory[0];
-    uint16_t* A = &sm->memory[1];
-    uint16_t* P = &sm->memory[2];
-    uint8_t opcode = sm->instructions[*PC] >> 4;
-    uint8_t* I = sm->instructions[*PC] & 0xF;
-    uint16_t* V = &sm->memory[P - I];
+    uint16_t *mem   = &vm->memory;
+    uint16_t *instr = &vm->instruction;
+    uint16_t *PC    = &mem[0];
+    uint8_t   OP    = instr[*PC] >> 4;
+    uint8_t   I4    = instr[*PC] & 0xF;
+    uint16_t *A     = &mem[1];
+    uint16_t *V     = &mem[2];
+    uint16_t  I16   = instr[*PC+1] << 8 | instr[*PC+2];
+    uint8_t   Z     = *A == 0;
+    uint8_t   N     = *A >> 15;
 
-    switch (opcode) {
-        case 0x0: 
-            *V = sm->instructions[*PC + 1] << 8 | sm->instructions[*PC + 2];
-            *PC += 2;
-            break;
-        case 0x1: 
-            break;
-        case 0x2: *A = *V; break;                     // Load
-        case 0x3: *V = *A; break;                     // Store
-        case 0x4: *A = sm->memory[*V]; break;         // Load Pointer
-        case 0x5: sm->memory[*V] = A; break;          // Store Pointer
-        case 0x6: *V = set_data(sm, *PC); break;      // Set
-        case 0x7: break;
-        case 0x8: break;
-        case 0x9: *A += *V; break;            // Add
-        case 0xA: *A -= *V; break;            // Sub
-        case 0xB: *A *= *V; break;            // Mul
-        case 0xC: *A /= *V; break;            // Div
-        case 0xD: *A &= *V; break;            // And
-        case 0xE: *A |= *V; break;            // Or
-        case 0xF: *A = ~*V; break;            // Not
-    }
+    switch (OP(vm)) {
+        case 0x0: *V = I16; *PC += 2;       break;
+        case 0x1:                           break;  // If
+        case 0x2: *A = *V;                  break;  // Load
+        case 0x3: *V = *A;                  break;  // Store
+        case 0x4: *A = mem[*V];             break;  // Load Pointer
+        case 0x5: mem[*V] = A;              break;  // Store Pointer
+        case 0x6:   break;
+        case 0x7:   break;
+        case 0x8:   break;
+        case 0x9: *A += *V;                 break;  // Add
+        case 0xA: *A -= *V;                 break;  // Sub
+        case 0xB: *A *= *V;                 break;  // Mul
+        case 0xC: *A /= *V;                 break;  // Div
+        case 0xD: *A &= *V;                 break;  // And
+        case 0xE: *A |= *V;                 break;  // Or
+        case 0xF: *A = ~*V;                 break;  // Not
+    }               
+
+    *PC += 1;
 }
 
-void run(SimplicMicrocontroller *sm) {
-    while (sm->memory[0] < 0xFFFF) {
-        execute(sm);
+void run(SimplicMicrocontroller *vm) {
+    uint16_t *mem = &vm->instr;
+    uint16_t *PC  = mem[0];
+    while (*PC < 0xFFFF) {
+        execute(vm);
     }
 }
 
