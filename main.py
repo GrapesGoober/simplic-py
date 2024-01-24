fc1173036b9ae86a1287013080e98dc09152ba10
grapsgoober <twinklierre@gmail.com>
2024-01-24 10:43:20 +0700

    wrote stdin for main.py

--- main.py
+++ main.py
@@ -2,48 +2,40 @@ from simplic import SimplicVM, SimplicIR, SimplicAsm, SimplicErr, error_print
 
 if __name__ == '__main__':
 
-
-
-
     from test_codes.IRtests import func_main, func_add_ten, fibbonaci
     ir = SimplicIR(fibbonaci, 'fibbonaci')
     asmcode = ir.compile()
     # ir = SimplicIR(func_add_ten, 'func_add_ten')
     # asmcode += ir.compile()
-
     
     asm = SimplicAsm()
     asm.from_list(asmcode)
     bytecodes = asm.compile()
     
-
-    with open("test_codes\\test.hex", 'w') as f:
-        for i, bytecode in enumerate(bytecodes):
-            f.write(f'{bytecode:02x} ' + ('\n' if (i + 1) % 16 == 0 else ''))
+    # # write to file
+    # with open("test_codes\\test.hex", 'w') as f:
+    #     for i, bytecode in enumerate(bytecodes):
+    #         f.write(f'{bytecode:02x} ' + ('\n' if (i + 1) % 16 == 0 else ''))
 
     # # handle error using
     # # error_print(asmfile, asm.iter, e.message)
     # [print(l) for l in asmcode]
     # [print(f"{b:02x}", end=' ') for b in bytecodes]
 
-    # vm = SimplicVM()
-    # vm.from_list(bytecodes)
-    # vm.run()
+    import subprocess
+    exe_path = "simplic\\virtualmachine\\virtualmachine.exe"
+    process = subprocess.Popen([exe_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    process.stdin.write(' '.join([f'{i:02x}' for i in bytecodes]))
+    process.stdin.close() 
 
-    # import subprocess
-    # # Step 2: Executing the Exe Program
-    # exe_path = "test_codes\\test.exe"
-    # process = subprocess.Popen([exe_path, "a", "b", "c"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            # Process the output as needed
+            print(output.strip())
 
-    # # Step 3: Reading Continuous Output from the Exe Program
-    # while True:
-    #     output = process.stdout.readline()
-    #     if output == '' and process.poll() is not None:
-    #         break
-    #     if output:
-    #         # Process the output as needed
-    #         print("Output:", output.strip())
-
-    # # Wait for the process to finish
-    # process.wait()
+    # Wait for the process to finish
+    process.wait()
 
