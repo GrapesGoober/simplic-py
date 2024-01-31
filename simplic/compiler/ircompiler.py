a2d3d3ea8919ecda008655d7021e1c38efc2a21e
grapsgoober <twinklierre@gmail.com>
2024-01-31 14:15:11 +0700

    refactored to reduce IR function compiler (untested)

--- simplic/compiler/ircompiler.py
+++ simplic/compiler/ircompiler.py
@@ -3,14 +3,9 @@ class SimplicIR:
     def __init__(self, ircode: list, name: str) -> None:
         self.alloc = {v:i+1 for i, v in enumerate(ircode[0])}
         self.name = name
-        self.code = ircode[1]
+        self.ircode = ircode[1]
         self.asm = [('label', self.name)]
         self.current_window = 0
-        self.return_count = 0
-    
-    # Get variable allocation relative to window
-    def get_alloc(self, var: str) -> int:
-        return self.alloc[var] % 16
     
     # Slide current window to the target variable location
     def slide(self, location: str) -> list[tuple[str]]:
@@ -20,51 +15,43 @@ class SimplicIR:
         elif delta > 0: return [('stack', 'push')] * delta
         else:           return []
     
-    def instr(self, op: str, var: str, imm: any = None) -> list[tuple[str]]:
-        if imm != None: return self.slide(self.alloc[var]) + [(op, self.get_alloc(var), imm)]
-        else:           return self.slide(self.alloc[var]) + [(op, self.get_alloc(var))]
-    
-    def instr_arg(self, op: str, offset: int, imm: any = None) -> list[tuple[str]]:
-        yield self.slide(len(self.alloc) + (offset + 1))
-        if imm != None: yield (op, (offset + 1) % 16, imm),
-        else:           yield (op, (offset + 1) % 16),
+    # Slide window and returns the allocation for variables or frame headers
+    def take(self, operand: int | str) -> int:
+        # handle 3 seperate cases, 
+        # 1) operand is variable
+        if isinstance(operand, str):  
+            self.asm += self.slide(self.alloc[operand])
+            return self.alloc[operand] % 16
+        # 2) operand is an absolute address (arguments or header)
+        elif operand >= 0: 
+            self.asm += self.slide(operand)
+            return operand % 16
+        # 3) operand is a header field of the next frame
+        elif operand < 0: 
+            self.asm += self.slide(len(self.alloc) - operand)
+            return (len(self.alloc) - operand) % 16
     
     # Compile entire IR code to ASM
     def compile(self) -> list[tuple[str]]:
-        for tokens in self.code:
+        for tokens in self.ircode:
             match tokens[0]:
-                case 'setargs': 
-                    for i, arg in enumerate(tokens[1:]):
-                        self.asm += self.instr('load', arg) + self.instr_arg('store', i)
-                case 'setrets': 
-                    for i, ret in enumerate(tokens[1:]):
-                        self.asm += self.instr_arg('load', i) + self.instr('store', ret)
-                case 'call':    
-                    return_label = f"{self.name}.return_{self.return_count}"
-                    self.asm += self.instr_arg('set', -1, return_label)
-                    self.asm += ('if', 'always', tokens[1]), ('label', return_label)
-                    self.return_count += 1
-                case 'return':  
-                    for i, ret in enumerate(tokens[1:]):
-                        self.asm += self.instr('load', ret)
-                        self.asm += self.instr('store', list(self.alloc)[i])
-                    self.asm += self.slide(0) + [('load', 0), ('set', 0, 0), ('storem', 0)]
-                case 'loadm' | 'storem':
-                    self.asm += self.instr('load', tokens[2])
-                    self.asm += self.instr(tokens[0], tokens[1])
                 case 'label':
                     self.asm += ('label', f"{self.name}.{tokens[1]}"),
                 case 'if':
                     self.asm += ('if', tokens[1], f"{self.name}.{tokens[2]}"),
+                case 'loadm' | 'storem':
+                    self.asm += ('load', self.take(tokens[2])), (tokens[0], self.take(tokens[1])),
                 case 'set':     
-                    self.asm += self.instr('set', tokens[1], tokens[2])
+                    self.asm += ('set', self.take(tokens[1]), tokens[2]),
                 case 'move':
-                    self.asm += self.instr('load', tokens[2]) + self.instr('store', tokens[1])
+                    self.asm += ('load', self.take(tokens[2])), ('store', self.take(tokens[1])),
                 case 'cmp':
-                    self.asm += self.instr('load', tokens[1]) + self.instr('sub', tokens[2])
+                    self.asm += ('load', self.take(tokens[1])), ('sub', self.take(tokens[2])),
                 case _:
-                    self.asm += self.instr('load', tokens[2])
-                    self.asm += self.instr(tokens[0], tokens[3])
-                    self.asm += self.instr('store', tokens[1])
+                    self.asm += [
+                        ('load', self.take(tokens[2])),
+                        (tokens[0], self.take(tokens[3])),
+                        ('store', self.take(tokens[1])),
+                    ]
 
         return self.asm
