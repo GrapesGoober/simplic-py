814d0c72024cf8496dd0ec46e15a05c05b493737
Graps <twinklierre@gmail.com>
2024-02-05 22:59:30 +0700

    implement rudementary IR parser

--- simplic/compiler/irparser.py
+++ simplic/compiler/irparser.py
@@ -1,5 +1,19 @@
 class SimplicIRParser:
     def from_file(self, filename: str) -> list[tuple]:
         with open(filename, 'r') as file:
-            for line in file:
-                print(line, end='')
+            for linenum, line in enumerate(file):
+                tokens = line.split('#')[0].split()
+                if tokens == []: continue
+
+                match tokens[0]:
+                    case 'func':
+                        print('\nDefining a new function')
+                        print('Name:', tokens[1])
+                    case 'label':
+                        print('label', tokens[1])
+                    case _:
+                        print('\t', ' '.join(tokens))
+
+
+SimplicIRParser().from_file("test_codes\\fib.ir")
+                        
