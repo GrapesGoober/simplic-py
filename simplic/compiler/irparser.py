d4c163c7c6e44c732e0b91411bf9cd3a11214998
grapsgoober <twinklierre@gmail.com>
2024-02-21 14:19:29 +0700

    implemented first stage parser

--- simplic/compiler/irparser.py
+++ simplic/compiler/irparser.py
@@ -14,27 +14,6 @@ def psuedo_ir_instruction(operator, dest, operand1, operand2):
 
 import re
 
-# syntax idea: 
-# an IF statement is defined as
-# if <cond>, label
-# then cond can be defined as
-# either    <var> <compare> <var>    or  <var>
-
-# try experimenting unnamed capture group
-word = r'\s*([\w.%]+)\s*'
-args = r'\(%s(?:,%s)*,?\)' % (word, word)
-
-patterns = [
-    ("FUNC",    r'func\s+([\w.%]+)'),
-    ("ARGS",    r'\(((\s*[\w.%]+\s*,?\s*)*)\)'),
-    ("LABEL",    r'([\w.%]+)\s*:'),
-    ("FUNC",    r'return\s+([\w.%]+)'),
-    ("FUNC",    r'([\w.%]+)\s*='),
-    ("FUNC",    r'call\s+([\w.%]+)'),
-    ("FUNC",    r'(\+|-|\*|\/|<<|>>|&|\||~)'),
-    ("FUNC",    r'(>|<|>=|<=|==|!=)'),
-    ("FUNC",    r'([\w.%]+)'),
-]
 patterns = [
     r'func\s+(?P<FUNC>[\w.%]+)',
     r'if\s+(?P<IF>[\w.%]+\s*(>|<|>=|<=|==|!=)\s*[\w.%]+)',
@@ -48,47 +27,26 @@ patterns = [
     r'(?P<OPERAND>[\w.%]+)',
 ]
 
-# we have 2 unparsed subtokens: IF's comparison and ARGS items
-# [('IF', 'n > %i0'), ('OPERAND', 'recurse')]
-# [('FUNC', 'test_arg_parser'), ('ARGS', 'a, b, c')]
+instr_pattern = r'((?P<DEST>.*)\s*=\s*)?(?P<OPERATION>.*)'
+goto_pattern = r'(if\s*(?P<COMPARISON>.*))?\s*goto\s*(?P<GOTO>.*)'
 
-# also should group generic "operations" together as one unit
-'''
-"FUNC": {
-    "NAME" : "funcname",
-    "ARGS" : [] | None
-}
-
-"LABEL": {
-    "NAME": "start"
-}
-
-"OPERATION": {
-    "LEFT" : None | str
-    "OPERATOR" : None | both binary, unary, and comparison
-    "RIGHT" : None | str
-}
-
-
-'''
+sample_code = [
+    "x = 3",
+    "y = 2",
+    "ptr a = b + c",
+    "goto label",
+    "if x > 3 goto label",
+    "call somefunc",
+]
 
 def tokenize_ir(ir_string: str):
-    index = 0
-    while index < len(ir_string):
-        token = None
-        for pattern in patterns:
+    m = re.match(goto_pattern, ir_string)
+    if not m: m = re.match(instr_pattern, ir_string)
+    if m: return m.groupdict()
+    else: return {}
+    
 
-            # # testing out setargs
-            # match_args = re.findall(word, ir_string[index:])
-            # for m in match_args:
-            #     print("\targs", m)
-
-            matched = re.match(f"\s*{pattern}\s*", ir_string[index:])
-            if matched != None:
-                index += matched.end()
-                token = list(matched.groupdict().items())[0]
-                break
-        if token: yield token
-        else: raise Exception(f'Unexpected token "{ir_string[index:]}"')
-
-SimplicIRParser().from_file("test_codes\\fib.ir")
+for linenum, line in enumerate(sample_code):
+    line = line.split("#")[0].lower()
+    if line.strip() == "": continue
+    print(tokenize_ir(line))
