789401bceb96bef62e05e66784cad795929d9e90
grapsgoober <twinklierre@gmail.com>
2024-03-22 16:32:14 +0700

    simplified VM (using only 2 functions: execute and main)

--- simplic/virtualmachine/virtualmachine.c
+++ simplic/virtualmachine/virtualmachine.c
@@ -7,73 +7,61 @@
 typedef uint16_t    word;   // default to 16 bit word size
 typedef uint8_t     byte;   // define a byte as an 8-bit word
 
-typedef struct SimplicVM {
-    word PC, A, SP;
-    word instr[SIZE], mem[SIZE];
-} SimplicVM;
+word A = 0, PC = 0, SP = 0xFFFF, program[SIZE], mem[SIZE];
 
-void execute(SimplicVM *vm); // step execute the current VM state
-void run    (SimplicVM *sm); // initialize and continuously run VM
+void execute() { // step execute the current VM state
 
-void main() {
-    SimplicVM *vm = (SimplicVM*)malloc(sizeof(SimplicVM));
-    init(vm); run(vm); print(vm); free(vm);
-}
+    // first, decode the current instruction
+    word instr = program[PC];
+    byte opcode = instr >> 12, imm = instr & 0xFF, cond = instr>>8 & 0x4;
+    word *V = mem[SP - imm];
 
-void execute(SimplicVM *vm) {
-
-    word *PC = &vm->PC, *A = &vm->A, *SP = &vm->SP;
-    word *mem = &vm->mem, instr = vm->instr[*PC];
-    byte opcode = instr >> 12, imm = instr & 0xFF, cond = instr >> 8 & 0x4;
-    word *V = mem[*SP - imm];
-
-    bool Z = vm->A == 0, N = vm->A >> 15;
+    // next, check the condition
+    bool Z = A == 0, N = A >> 15;
     bool flags[] = { 1, N, !(Z | N), Z, !Z, (Z | N), !N };
-
     if (flags[cond]) {
         // if true, executes depending on opcode
         switch (opcode) {
-            case 0x0: *A = *V;              break;  // Load
-            case 0x1: *V = *A;              break;  // Store
-            case 0x2: *A = mem[*V];         break;  // Load Pointer
-            case 0x3: mem[*V] = *A;         break;  // Store Pointer
-            case 0x4: *A +=  *V;            break;  // Add
-            case 0x5: *A -=  *V;            break;  // Sub
-            case 0x6: *A <<= *V;            break;  // LSL
-            case 0x7: *A >>= *V;            break;  // LSR
-            case 0x8: *A *=  *V;            break;  // Mul
-            case 0x9: *A /=  *V;            break;  // Div
-            case 0xA: *A &=  *V;            break;  // And
-            case 0xB: *A |=  *V;            break;  // Or
-            case 0xC: *A =  ~*V;            break;  // Not
+            case 0x0: A = *V;              break;  // Load
+            case 0x1: *V = A;              break;  // Store
+            case 0x2: A = mem[*V];         break;  // Load Pointer
+            case 0x3: mem[*V] = A;         break;  // Store Pointer
+            case 0x4: A +=  *V;            break;  // Add
+            case 0x5: A -=  *V;            break;  // Sub
+            case 0x6: A <<= *V;            break;  // LSL
+            case 0x7: A >>= *V;            break;  // LSR
+            case 0x8: A *=  *V;            break;  // Mul
+            case 0x9: A /=  *V;            break;  // Div
+            case 0xA: A &=  *V;            break;  // And
+            case 0xB: A |=  *V;            break;  // Or
+            case 0xC: A =  ~*V;            break;  // Not
             // TODO: slide up and down with MSB as direction and 7 bits as distance
-            case 0xD: *SP += imm; *A = *SP; break;  // Stack Slide
+            case 0xD: SP += imm; A = SP; break;  // Stack Slide
             case 0xE: *V = *V<<8 | imm;     break;  // Insert
-            case 0xF: *PC = *V;             break;  // Goto                                        
+            case 0xF: PC = *V;             break;  // Goto                                        
         }  
     }
-    // lastly, increment program counter
-    *PC += 1;
+    // lastly, increment program counter for the next instruction
+    PC += 1;
 }
 
-void run(SimplicVM *vm) {
+int main() {
 
-    vm->mem[0] = 0, vm->mem[2] = 0xFFFF;
-    word count = 0; 
-    while (scanf("%2hhx", &vm->instr[count++]) == 1);
+    word count = 0; // initialize program from stdin
+    while (scanf("%2hhx", &program[count++]) == 1);
 
-    while (vm->mem[0] != 0xFFFF) {
-        execute(vm);
-        switch (vm->mem[3]) {
-            case 1:  printf("%c", vm->mem[4]); break;      // output to stdout
-            case 2:  scanf("%c", &vm->mem[4]); break;      // recieve character from stdin
-        }
-        vm->mem[3] = 0;
+    while (PC != 0xFFFF) { // run VM with IO and halt condition
+        execute();
+        if (mem[0] == 1) printf("%c", mem[1]); // output to stdout
+        if (mem[0] == 2) scanf("%c", &mem[1]); // recieve character from stdin
+        mem[0] = 0;
     }
 
-    printf("PC\t%d\n", vm->PC);
+    printf("PC\t%d\n", PC);
     printf("stack\n");
     for (int i = 0; i < 0x10; i++) {
-        printf("  %x\t%d\n", i, vm->mem[vm->SP - i]);
+        printf("  %x\t%d\n", i, mem[SP - i]);
     }
+
+    return 0;
 }
