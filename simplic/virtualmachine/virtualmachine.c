fe6d49f31173a08dd5810449f1b05e2e2de2cf43
grapsgoober <twinklierre@gmail.com>
2024-03-25 13:11:58 +0700

    renamed global variables to use CAPS

--- simplic/virtualmachine/virtualmachine.c
+++ simplic/virtualmachine/virtualmachine.c
@@ -7,14 +7,15 @@
 typedef uint16_t    word;   // default to 16 bit word size
 typedef uint8_t     byte;   // define a byte as an 8-bit word
 
-word A = 0, PC = 0, SP = 0xFFFF, program[SIZE], mem[SIZE];
+// Simplic is a word-RAM model, Harvard architecture, accumulator-based machine
+word A = 0, PC = 0, SP = 0xFFFF, PROG[SIZE], MEM[SIZE];
 
 void execute() { // step execute the current VM state
 
     // first, decode the current instruction
-    word instr = program[PC];
+    word instr = PROG[PC];
     byte opcode = instr >> 12, imm = instr & 0xFF, cond = instr>>8 & 0x4;
-    word *V = mem[SP - imm];
+    word *V = MEM[SP - imm];
 
     // next, check the condition
     bool Z = A == 0, N = A >> 15;
@@ -24,8 +25,8 @@ void execute() { // step execute the current VM state
         switch (opcode) {
             case 0x0: A = *V;               break;  // Load
             case 0x1: *V = A;               break;  // Store
-            case 0x2: A = mem[*V];          break;  // Load Pointer
-            case 0x3: mem[*V] = A;          break;  // Store Pointer
+            case 0x2: A = MEM[*V];          break;  // Load Pointer
+            case 0x3: MEM[*V] = A;          break;  // Store Pointer
             case 0x4: A +=  *V;             break;  // Add
             case 0x5: A -=  *V;             break;  // Sub
             case 0x6: A <<= *V;             break;  // LSL
@@ -35,7 +36,6 @@ void execute() { // step execute the current VM state
             case 0xA: A &=  *V;             break;  // And
             case 0xB: A |=  *V;             break;  // Or
             case 0xC: A =  ~*V;             break;  // Not
-            // TODO: slide up and down with MSB as direction and 7 bits as distance
             case 0xD: SP += imm; A = SP;    break;  // Stack Slide
             case 0xE: *V = *V<<8 | imm;     break;  // Insert
             case 0xF: PC = *V;              break;  // Goto                                        
@@ -49,20 +49,20 @@ int main() {
 
     // first, initialize program from stdin
     word count = 0;
-    while (scanf("%2hhx", &program[count++]) == 1);
+    while (scanf("%2hhx", &PROG[count++]) == 1);
 
     // next run VM with IO until reached the halt condition
     while (PC != 0xFFFF) { 
         execute();
-        if (mem[0] == 1) printf("%c", mem[1]); // output to stdout
-        if (mem[0] == 2) scanf("%c", &mem[1]); // recieve character from stdin
-        mem[0] = 0;
+        if (MEM[0] == 1) printf("%c", MEM[1]); // output to stdout
+        if (MEM[0] == 2) scanf("%c", &MEM[1]); // recieve character from stdin
+        MEM[0] = 0;
     }
 
     printf("PC\t%d\n", PC);
     printf("stack\n");
     for (int i = 0; i < 0x10; i++) {
-        printf("  %x\t%d\n", i, mem[SP - i]);
+        printf("  %x\t%d\n", i, MEM[SP - i]);
     }
 
     return 0;
