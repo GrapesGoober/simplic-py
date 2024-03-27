de9ea4acb0cb7148b2ecd920f662c3967abeedd6
grapsgoober <twinklierre@gmail.com>
2024-03-27 12:56:02 +0700

    formatted execute switch-case

--- simplic/virtualmachine/virtualmachine.c
+++ simplic/virtualmachine/virtualmachine.c
@@ -23,23 +23,23 @@ void execute() { // step execute the current VM state
     if (flags[cond]) {
         // if true, executes depending on opcode
         switch (opcode) {
-            case 0x0: A = *V;               break;  // Load
-            case 0x1: *V = A;               break;  // Store
-            case 0x2: A = MEM[*V];          break;  // Load Pointer
-            case 0x3: MEM[*V] = A;          break;  // Store Pointer
-            case 0x4: A +=  *V;             break;  // Add
-            case 0x5: A -=  *V;             break;  // Sub
-            case 0x6: A <<= *V;             break;  // LSL
-            case 0x7: A >>= *V;             break;  // LSR
-            case 0x8: A *=  *V;             break;  // Mul
-            case 0x9: A /=  *V;             break;  // Div
-            case 0xA: A &=  *V;             break;  // And
-            case 0xB: A |=  *V;             break;  // Or
-            case 0xC: A =  ~*V;             break;  // Not
-            case 0xD: A = A<<8 | I8;    break;  // Insert
-            case 0xE: PC = *V;          break;  // Goto                                        
-            case 0xF: SP += I8 - 127;      
-                      A = SP;           break;  // Stack Slide
+            case 0x0: A  = *V;          break;  // Load
+            case 0x1: *V = A;           break;  // Store
+            case 0x2: A  = MEM[*V];     break;  // Load Pointer
+            case 0x3: MEM[*V] = A;      break;  // Store Pointer
+            case 0x4: A +=  *V;         break;  // Add
+            case 0x5: A -=  *V;         break;  // Sub
+            case 0x6: A <<= *V;         break;  // LSL
+            case 0x7: A >>= *V;         break;  // LSR
+            case 0x8: A *=  *V;         break;  // Mul
+            case 0x9: A /=  *V;         break;  // Div
+            case 0xA: A &=  *V;         break;  // And
+            case 0xB: A |=  *V;         break;  // Or
+            case 0xC: A  = ~*V;         break;  // Not
+            case 0xD: A  = A<<8 | I8;   break;  // Insert
+            case 0xE: PC = *V;          break;  // Goto 
+            case 0xF: SP += I8 - 127;           // Stack Slide
+                      A  = SP;          break;  
         }  
     }
     // lastly, increment program counter for the next execution
